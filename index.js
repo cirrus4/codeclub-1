851a8929501f4d18e85529a0dce360bc632c4998
Christian Steiger <csteiger@cloudflare.com>
2022-11-08 11:58:28 +0100

    assignment 1

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 export default {
-	fetch() {
-		return new Response('Hello worker!', {
-			headers: {
-				'content-type': 'text/plain',
-			},
-		});
+	fetch(request) {
+		let output = 'Hello worker!';
+		const method = request.method;
+		let headers = { 
+						headers: {
+							'content-type': 'text/plain',
+						}
+					};
+
+		if(method == 'POST'){
+			output = "{body: 'Hello POST Worker!' }";
+			headers.headers = {'content-type': 'application/json;charset=UTF-8'}
+			output = JSON.stringify(output);
+		}else if(method == 'GET'){
+			output = 'Hello GET Worker!';
+		}
+
+		return new Response(output, headers);
 	},
 };
