f62d8ac870dd173b5781ebdf3c2098029dff58f9
Christian Steiger <csteiger@cloudflare.com>
2022-11-10 09:10:47 +0100

    add coding tipps

--- index.js
+++ index.js
@@ -2,20 +2,16 @@ export default {
 	fetch(request) {
 		let output = 'Hello worker!';
 		const method = request.method;
-		let headers = { 
-						headers: {
-							'content-type': 'text/plain',
-						}
-					};
+		const responseOptions = {headers: {'content-type': 'text/plain',}};
 
 		if(method == 'POST'){
-			output = "{body: 'Hello POST Worker!' }";
-			headers.headers = {'content-type': 'application/json;charset=UTF-8'}
+			output = {body: 'Hello POST Worker!' };
+			responseOptions.headers = {'content-type': 'application/json;charset=UTF-8'}
 			output = JSON.stringify(output);
 		}else if(method == 'GET'){
 			output = 'Hello GET Worker!';
 		}
 
-		return new Response(output, headers);
+		return new Response(output, responseOptions);
 	},
 };
